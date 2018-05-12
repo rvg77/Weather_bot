90a7df11a3506225d70bf3335ade5c0fbabf5bae
rvg_77 <gorb.roman1999@gmail.com>
2018-05-12 15:16:14 +0300

    Changed messages.
    Signed-off-by: rvg_77 <gorb.roman1999@gmail.com>

--- commands.py
+++ commands.py
@@ -4,17 +4,11 @@ import images
 
 def start_command(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text='Введите /help чтобы понять как работает бот.'
-                          '\n\n'
-                          'Type /help to find out how bot works.')
+                     text='Welcome! We are glad to see you here!\nType /help to find out how bot works.')
 
 
 def help_command(bot, update):
-    response = 'Чтобы посмотреть текущую погоду, введите название города ' \
-               'на русском или английском языке.\n' \
-               'Note: \nДобавьте в конец "ru" или другой двухбуквенный ' \
-               'псевдоним страны для точности.\n\n' \
-               'Type a name of the city to watch current weather' \
+    response = 'Type a name of the city to watch current weather' \
                '(in Russian or in English).\n' \
                'Note: \nYou can add "ru" or other 2-letter name of the country to the and of request.'
 
@@ -27,8 +21,7 @@ def text_message_command(bot, update):
         response, status = weather.info(city)
     except BaseException:
         bot.send_message(chat_id=update.message.chat_id,
-                         text='Incorrect city. Please, try again.\n\n'
-                              'Некорректный город. Пожалуйста, попробуйте еще раз.')
+                         text='Incorrect city. Please, try again.')
         return
     bot.send_message(chat_id=update.message.chat_id, text=response)
     try:
